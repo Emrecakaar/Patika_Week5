94510c5bb56d285ab2d52fa795d59a0ceada609c
Emre Cakar <152209307+Emrecakaar@users.noreply.github.com>
2024-01-15 14:47:18 +0100

    Add files via upload

---
+++ LIKE - ILIKE.sql
@@ -0,0 +1,5 @@
+-- SELECT country FROM country WHERE country LIKE 'A%a';
+
+-- SELECT country FROM country WHERE country ILIKE '_____%n'
+-- SELECT title FROM film WHERE title ILIKE '%t%';
+-- SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
